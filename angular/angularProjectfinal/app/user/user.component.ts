863a336fc37249445447ee0f2350e24b39d1cfa3
work123acc <56397496+work123acc@users.noreply.github.com>
2021-08-19 09:12:33 +0300

    Add files via upload

---
+++ angular/angularProjectfinal/app/user/user.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core'; 
+
+@Component({
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.scss'],
+})
+
+export class UserComponent {
+	
+	@Input() user;
+	isMarked = false;
+	
+	onClick() {
+		console.log('Clicked!');
+		this.isMarked = true;
+	}
+	
+	
+}
